089f5fdc4dcb420d639af3657731eb882050631a
Dheeraj <410dheeraj@gmail.com>
2020-09-06 03:24:09 +0530

    refactor(clean up): removed unnessery code

--- site/gatsby-browser.js
+++ site/gatsby-browser.js
@@ -1,9 +1,1 @@
 import "./src/styles/index.css"
-
-// exports.onClientEntry = () => {
-//   // IntersectionObserver polyfill for gatsby-image (Safari, IE)
-//   if (typeof window.IntersectionObserver === `undefined`) {
-//     import(`intersection-observer`)
-//     console.log(`👍 IntersectionObserver is polyfilled`)
-//   }
-// }
