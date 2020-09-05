9c21d61097e842e61c5834083356a464ddea215a
Dheeraj <410dheeraj@gmail.com>
2020-09-05 21:31:42 +0530

    feat(Header): Added new responsive header component

--- site/src/containers/Header/index.tsx
+++ site/src/containers/Header/index.tsx
@@ -3,9 +3,13 @@ import { Logo, Nav } from "../../components"
 
 export const Header = () => {
   return (
-    <div className="text-xl">
-      <Logo />
-      <Nav />
-    </div>
+    <header className="w-screen text-xl inline-flex justify-start items-center px-3">
+      <div className="ml-0 md:ml-10 lg:ml-24">
+        <Logo />
+      </div>
+      <div className="flex flex-grow justify-center">
+        <Nav />
+      </div>
+    </header>
   )
 }
