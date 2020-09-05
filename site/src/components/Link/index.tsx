ae4329b68cc5a4b8d84fbb8b1fb6728877866805
Dheeraj <410dheeraj@gmail.com>
2020-09-05 21:29:17 +0530

    feat(Link): added link component

--- site/src/components/Link/index.tsx
+++ site/src/components/Link/index.tsx
@@ -4,5 +4,14 @@ import AnchorLink from "react-anchor-link-smooth-scroll"
 export const Link = (props: LinkProps) => {
   const { text, to } = props
 
-  return <AnchorLink href={to}>{text}</AnchorLink>
+  return (
+    <AnchorLink href={to} className="hover:text-wine">
+      {text}
+    </AnchorLink>
+  )
+}
+
+declare interface LinkProps {
+  text: string
+  to: string
 }
