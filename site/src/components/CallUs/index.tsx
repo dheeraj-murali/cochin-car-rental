10334659ea91de57cc17286ffedb17d221383445
Dheeraj <410dheeraj@gmail.com>
2020-09-07 17:56:13 +0530

    feat(Callus): added big fat phone number

---
+++ site/src/components/CallUs/index.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+export const CallUs = (props: CallUsProps) => {
+  const { phone } = props
+
+  return (
+    <div className="flex flex-col items-center justify-center max-w-3xl py-5">
+      <p className="text-white uppercase font-light p-3">call us at</p>
+      <hr className="border border-white w-1/2" />
+      <a
+        href={`tel:${phone}`}
+        className="text-white text-4xl p-3 hover:text-black"
+      >
+        {phone}
+      </a>
+      <hr className="border border-white w-1/2" />
+      <p className="text-white uppercase font-light p-3">
+        pre book your taxi now
+      </p>
+    </div>
+  )
+}
+
+declare interface CallUsProps {
+  phone: string
+}
