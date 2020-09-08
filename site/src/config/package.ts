0a8dc01d52a5a6a89c4817d66469c0600210c6ff
Dheeraj <410dheeraj@gmail.com>
2020-09-08 19:30:05 +0530

    feat(packages): Configurable packages
    - added new cards to package section
    - added updated configuration with images

--- site/src/config/package.ts
+++ site/src/config/package.ts
@@ -1,5 +1,19 @@
-export const config = {
-  title: `Special package`,
-  subtitle: `Are you planning a trip to Sabarimala on this season?`,
-  content: `If you're looking for reliable car rental options in Kochi, Cochin Car Rentals provides a wide variety of taxis you can choose from. Whether you're looking for extra leg room or a luggage carrier, you can pick from any of our Sedan, SUV or Minivan options.`,
-}
+export const config = packages =>
+  packages.map(image => {
+    const name = image.node.name
+    const banner = image.node.childImageSharp.fluid
+
+    if (name === "package1")
+      return {
+        title: `Are you planning a trip to Sabarimala on this season?`,
+        content: `If you're looking for reliable car rental options in Kochi, Cochin Car Rentals provides a wide variety of taxis you can choose from. Whether you're looking for extra leg room or a luggage carrier, you can pick from any of our Sedan, SUV or Minivan options.`,
+        banner,
+      }
+
+    if (name == "package2")
+      return {
+        title: `Take a dream trip to Munnar`,
+        content: `We provide reliable car rental options from Kochi to the relaxing wilderness of Munnar tea estates. With a verity of rental options to choose from, we guarantee to provide the best service available for you.`,
+        banner,
+      }
+  })
