ab4b6b7190b5a1918615ffa672bb5a738e392b8d
Dheeraj <410dheeraj@gmail.com>
2020-09-20 11:04:43 +0530

    feat(social): added social link component

---
+++ site/src/components/Social/index.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaTwitterSquare,
+  FaWhatsappSquare,
+  FaLinkedin,
+} from "react-icons/fa"
+
+export const Social = (props: SocialProps) => {
+  const { link, item } = props
+
+  const getIcon = (
+    item: "facebook" | "twitter" | "instagram" | "whatsApp" | "linkedIn"
+  ) => {
+    if (item === "facebook") return <FaFacebookSquare />
+    if (item === "twitter") return <FaTwitterSquare />
+    if (item === "instagram") return <FaInstagramSquare />
+    if (item === "whatsApp") return <FaWhatsappSquare />
+    if (item === "linkedIn") return <FaLinkedin />
+  }
+
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-3xl text-white hover:text-wine"
+    >
+      {getIcon(item)}
+    </a>
+  )
+}
+
+declare interface SocialProps {
+  link: string
+  item: "facebook" | "twitter" | "instagram" | "whatsApp" | "linkedIn"
+}
